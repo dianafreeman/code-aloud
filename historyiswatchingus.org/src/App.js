c6744daa3ef0307f436ad10914dc51e853e910b7
Diana <dev@dianasteakleyfreeman.com>
2018-09-16 02:27:45 -0500

    fixed merge issues

--- historyiswatchingus.org/src/App.js
+++ historyiswatchingus.org/src/App.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
-import Rep from './components/person.jsx';
-import './index.css';
+import Head from './components/header.jsx';
+import StateSelect from './components/state-select.jsx'
+import Legislators from './components/legislators.jsx';
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware } from 'redux'
 import './assets/css/App.css';
-import repstyle from './assets/css/rep-card.css';
-import './assets/css/new-age.css';
 import './assets/css/new-age.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/bootswatch-litera.min.css';
+import {fetchLegislators} from './assets/js/fetchLegislators.js'
+{/*import {initMap} from './assets/js/map-vanilla.js';*/}
+{/* import SimpleMap from './assets/js/map.js'; */}
+
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setCurrentLocation = this.setCurrentLocation.bind(this);
+    this.setState({
+      'currentLocation': null,
+      'legislators': null
+    })
+  }
+ 
+
+ setCurrentLocation(newValue) {
+    console.log('this is the State code: ' + newValue);
+    this.setState({'currentLocation': newValue});
+
+  }
+
+ onSubmit(e){
+  e.preventDefault()
+  //console.log(this.state.currentLocation)
+ }
+
+
   render() {
     return (
-      <div>
-      <Head />
-        <h1>History Is Watching Us</h1>
-        <Download />
-          <Rep style={repstyle} />        
-      
-      </div>
+      <Provider store={store}>
+        <div>
+        <Head />  
+            <div className="text-center">
+              <form onSubmit={this.onSubmit}>
+                  <StateSelect />
+                  <button type="submit" className="btn btn-action"> Go! </button>
+              </form>
+            </div>
+        <Legislators />
+        </div>
+      </Provider>
     );
   }
 }
@@ -24,111 +59,4 @@ class App extends Component {
 export default App;
 
 
-class Head extends Component{
-  render(){
-    return (
-  <header className="masthead">
-      <div className="container h-100">
-        <div className="row h-100">
-          <div className="col-lg-7 my-auto">
-            <div className="header-content mx-auto">
-              <h1 className="mb-5">New Age is an app landing page that will help you beautifully showcase your new mobile app, or anything else!</h1>
-              <a href="#download" className="btn btn-outline btn-xl js-scroll-trigger">Start Now for Free!</a>
-            </div>
-          </div>
-          <div className="col-lg-5 my-auto">
-            <div className="device-container">
-              <div className="device-mockup iphone6_plus portrait white">
-                <div className="device">
-                  <div className="screen">
-            {/* Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! */}                  
-                <img src="img/demo-screen-1.jpg" className="img-fluid" alt="" />
-                  </div>
-                  <div className="button">
-              {/* You can hook the "home button" to some JavaScript events or just remove it */}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    );
-  }
 
-}
-
-class Nav extends Component {
-  render(){
-    return (
-<nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">History is Watching Us</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#download">Download</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#features">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>  
-    );
-  }
-  
-}
-
-class Download extends Component {
-  render(){
-    return (
-
-    <section className="download bg-primary text-center" id="download">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 mx-auto">
-            <h2 className="section-heading">Discover what all the buzz is about!</h2>
-            <p>Our app is available on any mobile device! Download now to get started!</p>
-            <div className="badges">
-              <a className="badge-link" href="#"><img src="img/google-play-badge.svg" alt="" /></a>
-              <a className="badge-link" href="#"><img src="img/app-store-badge.svg" alt="" /></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-      )
-
-
-  }
-
-
-}
-
-{/*class Download extends Component {
-  render(){
-    return (
-
-
-
-
-
-
-
-      )
-
-
-  }
-
-
-}*/}
--- historyiswatchingus.org/src/index.js
+++ historyiswatchingus.org/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import { Provider } from 'react-redux'
+import store from './components/store'
 import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+	 <App />
+	 </Provider>, document.getElementById('root'));
 registerServiceWorker();
